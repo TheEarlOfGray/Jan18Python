e039cb7be0491088cc67e27c7478ca22583033b1
Earl <earl.gray@qa.com>
2023-01-20 15:33:08 +0000

    EG: added filesIO content

--- carSale.py
+++ carSale.py
@@ -4,11 +4,11 @@ with open("carSale.csv", "r") as file1:
 for line in lines:
     lines[lines.index(line)] = line.split(",")
 
-
-
 months = lines.pop(0)
 
-
+for month in months:
+    if "\n" in month:
+        months[months.index(month)] = month[0:3]
 
 for line in lines:
     for lin in line:
@@ -17,8 +17,46 @@ for line in lines:
         else:
             line[line.index(lin)] = int(lin)
 
+
+
+# company = []
+
+# for indItem in lines:
+#     company.append(indItem.pop(0))
+    
+# print(company)
+
 print(months)
 print(lines)
 
-dict1 = {"Ford Motor Company": [16629, 10390, 40755, 18074, 19892, 22049, 17049, 10764]}
-print(sum(dict1["Ford Motor Company"]))
+
+
+dict1 = {}
+
+for listItem in lines:
+    dict1[listItem[0]] = listItem[1:] # Here I have used list 'slicing'.
+
+print(dict1)
+
+for item in dict1.keys():
+    if item == "Ford Motor Company":
+        result = sum(dict1[item])
+
+print(result)
+
+
+result2 = 0
+
+for item in dict1.keys():
+    result2 += dict1[item][months.index("May")]
+
+print(result2)
+
+result3 = 0
+
+for item in dict1.keys():
+    result3 += dict1[item][months.index("Aug")]
+
+result3 = result3 / len(dict1.keys())
+print(result3)
+
