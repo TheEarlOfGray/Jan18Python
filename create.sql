003d41ca20e0762e4fdab4f8c4b45010f00c7ce1
Earl <earl.gray@qa.com>
2023-01-24 15:22:51 +0000

    EG: added primary key

--- create.sql
+++ create.sql
@@ -1,19 +1,21 @@
 CREATE TABLE person (
+id INT UNIQUE,
 first_name VARCHAR(40),
 surname VARCHAR(40),
-age INT
+age INT,
+PRIMARY KEY id
 );
 
 INSERT INTO person 
 VALUES 
-('Sally', 'Smith', 26),
-('Dave', 'Smith', 31),
-('Simon', 'Smith', 20),
-('Tim', 'Smith', 21),
-('Fiona', 'Smith', 27),
-('Sophie', 'Smith', 27),
-('Peter', 'Smith', 30),
-('Stewart', 'Smith', 34),
-('Jane', 'Smith', 50)
+(1, 'Sally', 'Smith', 26),
+(2, 'Dave', 'Smith', 31),
+(3, 'Simon', 'Smith', 20),
+(4, 'Tim', 'Smith', 21),
+(5, 'Fiona', 'Smith', 27),
+(6, 'Sophie', 'Smith', 27),
+(7, 'Peter', 'Smith', 30),
+(8, 'Stewart', 'Smith', 34),
+(9, 'Jane', 'Smith', 50)
 ;
 
